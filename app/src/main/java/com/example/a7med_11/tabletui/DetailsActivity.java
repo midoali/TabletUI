b588f368a3d7afd0c4894f6b695006d033724850
Ahmed Ali <aaliebrahim@fcih1.com>
2016-04-06 21:01:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a7med_11/tabletui/DetailsActivity.java
@@ -0,0 +1,38 @@
+package com.example.a7med_11.tabletui;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+/**
+ * Created by a7med_11 on 30/01/16.
+ */
+public class DetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check if the device is in landscape mode
+        if (getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE) {
+            // If the screen is now in landscape mode, we can show the
+            // dialog in-line with the list so we don't need this activity.
+            finish();
+            return;
+        }
+
+        // Check if we have any hero data saved
+        if (savedInstanceState == null) {
+
+            // If not then create the DetailsFragment
+            DetailsFragment details = new DetailsFragment();
+
+            // Get the Bundle of key value pairs and assign them to the DetailsFragment
+            details.setArguments(getIntent().getExtras());
+
+            // Add the details Fragment
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
+}
